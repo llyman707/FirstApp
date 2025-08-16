45dd69dde00948af40c31e4135ba7d4de4484c4f
llyman707 <llyman707@gmail.com>
2025-08-16 15:58:12 -0400

    setting up responses

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    console.log("we got a new request!")
-    res.send("Hello, we got your response")
+// app.use((req, res) => {
+//     console.log("we got a new request!")
+//     res.send("Hello, we got your response")
+// })
+app.get('/', (req, res) => {
+    res.send("This is a home page!")
+})
+
+app.post('cats', (res, req) => {
+    res.send("post request to/catsss!!!")
+})
+
+app.get('/cats', (req, res) => {
+    res.send("MEOW")
+})
+app.get('/dogs', (req, res) => {
+    res.send("WOOF")
+})
+app.get(/(.*)/, (req, res) => {
+    res.send(`I don't know that path`)
 })
 
 app.listen(3000, () => {
