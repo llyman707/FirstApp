09df0a91fcadf41ad5cb9a0a72367a137a6aa865
llyman707 <llyman707@gmail.com>
2025-08-16 15:35:38 -0400

    setting up requests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+
+app.use((req, res) => {
+    console.log("we got a new request!")
+    res.send("Hello, we got your response")
+})
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000")
+})
